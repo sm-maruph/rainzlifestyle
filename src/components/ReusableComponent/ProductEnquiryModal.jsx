b7de8e22c3163b6c6c2827ec2e3ee32d37b2971e
sm-maruph <62034743+sm-maruph@users.noreply.github.com>
2025-08-14 02:52:57 +0600

    recaptcha info updated

--- src/components/ReusableComponent/ProductEnquiryModal.jsx
+++ src/components/ReusableComponent/ProductEnquiryModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const FORMSPREE_ENDPOINT = "https://formspree.io/f/movlkjea";
+const FORMSPREE_ENDPOINT = "https://formspree.io/f/xwpqzkvg";
 
 const ProductEnquiryModal = ({ isOpen, onClose, product }) => {
   const [formData, setFormData] = useState({
